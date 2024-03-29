e032277d0809eecd0f992361fa78d4ad0315d68e
Ajay <you@example.com>
2024-03-29 21:44:11 +0530

    backend successful

--- src/routes/signupRoute.js
+++ src/routes/signupRoute.js
@@ -2,19 +2,24 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 const bcrypt = require("bcrypt");
+const bodyParser = require("body-parser");
 
 const router = express.Router();
-app.use(express.json());
 
-const User = require("../models/user"); // Assuming you have a Mongoose model for users
+// Use body-parser middleware for both JSON and URL-encoded data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// This should be before any routes that expect request data
 
-// Mongoose will handle the connection internally, no need to create a new MongoClient instance
+const User = require("../models/user");
 
 const uri = process.env.MONGODB_URI;
 
 router.post("/signup", async (req, res) => {
-  console.log(req.body.firstName);
+  // Access parsed body data after middleware application
+  console.log(req.body);
   const userData = req.body;
+  console.log(userData)
 
   if (
     !userData.firstName ||
@@ -31,34 +36,20 @@ router.post("/signup", async (req, res) => {
     return res.status(400).send("Passwords do not match.");
   }
 
-  // Replace with actual user creation logic using your database/user management system
-  // This is a placeholder for security reasons (not storing passwords in plain text)
   try {
-    // await createNewUser(newUser); // Replace with your database interaction logic
-    // ...
-
     const password = userData.password;
-    let hashedPassword = "";
+    const hashedPassword = await bcrypt.hash(password, 10); // Hash the password
 
-    bcrypt.genSalt(10, (err, salt) => {
-      bcrypt.hash(password, salt, async (err, hash) => {
-        if (err) throw err;
-        hashedPassword = hash;
-
-        console.log(hashedPassword);
-        const newUser = new User({
-          firstName: userData.firstName,
-          lastName: userData.lastName,
-          email: userData.email,
-          phone: userData.phone,
-          password: hashedPassword, // Placeholder for hashed password
-        });
-
-        await newUser.save(); // Save the new user to the database
-      });
+    const newUser = new User({
+      firstName: userData.firstName,
+      lastName: userData.lastName,
+      email: userData.email,
+      phone: userData.phone,
+      password: hashedPassword,
     });
 
-    // ...
+    await newUser.save(); // Save the new user to the database
+
     res.status(201).send("Signup successful!");
   } catch (error) {
     console.error(error);
@@ -67,9 +58,4 @@ router.post("/signup", async (req, res) => {
       .send("An error occurred during signup. Please try again later.");
   }
 });
-
 module.exports = router;
-
-// app.listen(3000, () => {
-//   console.log('Server is running on port 3000');
-// });
